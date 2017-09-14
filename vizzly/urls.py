52bd4583020494001015144716f2f8858576bb10
Nitesh Rijal <rijal.it@gmail.com>
2017-09-14 21:39:10 +0545

    some other graphs plotted

--- vizzly/urls.py
+++ vizzly/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from .views import index, single
+from .views import index, single, sub1, sub2
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^~admin/', admin.site.urls),
     url(r'^$', index, name='home'),
     url(r'^single/$', single, name='single'),
+    url(r'^sub1/$', sub1, name='sub1'),
+    url(r'^sub2/$', sub2, name='sub2'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 ]
--- vizzly/views.py
+++ vizzly/views.py
@@ -24,10 +24,58 @@ def single(request):
     is_admin = True if request.user.is_superuser else False
     username = request.user.username
 
+    # some data here
+    x = [1, 3, 5, 7, 9, 11, 13]
+    y = [1, 2, 3, 4, 5, 6, 7]
+    title = 'y = f(x)'
+
+    # plot params
+    plot = figure(title=title,
+                  x_axis_label='X-Axis',
+                  y_axis_label='Y-Axis',
+                  plot_width=400,
+                  plot_height=400)
+
+    # some plot function
+    plot.line(x, y, legend='f(x)', line_width=2)
+
+    # plot = figure()
+    # plot.circle([1, 2], [3, 4])
+
+    script, div = components(plot, CDN)
+
+    context = {'is_loggedin': is_loggedin, 'is_admin': is_admin, 'username': username, 'script': script, 'div': div}
+    return render(request, 'single.html', context)
+
+
+@login_required(login_url='/login/')
+def sub1(request):
+    is_loggedin = True if request.user.is_authenticated else False
+    is_admin = True if request.user.is_superuser else False
+    username = request.user.username
+
     plot = figure()
     plot.circle([1, 2], [3, 4])
 
     script, div = components(plot, CDN)
 
     context = {'is_loggedin': is_loggedin, 'is_admin': is_admin, 'username': username, 'script': script, 'div': div}
-    return render(request, 'single.html', context)
+    return render(request, 'global.html', context)
+
+
+@login_required(login_url='/login/')
+def sub2(request):
+    is_loggedin = True if request.user.is_authenticated else False
+    is_admin = True if request.user.is_superuser else False
+    username = request.user.username
+
+    # select the tools we want
+    TOOLS = "pan,save"
+
+    plot = figure(tools=TOOLS, plot_width=250, plot_height=200)
+    plot.circle([1, 7], [5, 9])
+
+    script, div = components(plot, CDN)
+
+    context = {'is_loggedin': is_loggedin, 'is_admin': is_admin, 'username': username, 'script': script, 'div': div}
+    return render(request, 'global.html', context)
