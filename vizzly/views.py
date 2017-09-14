0d56d09b3c16035d7caabf6b19872715cfe6ddfb
Nitesh Rijal <rijal.it@gmail.com>
2017-09-14 21:26:31 +0545

    working static plot example

--- vizzly/views.py
+++ vizzly/views.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import components
+
 
 @login_required(login_url='/login/')
 def index(request):
@@ -12,12 +17,17 @@ def index(request):
     context = {'is_loggedin': is_loggedin, 'is_admin': is_admin, 'username': username}
     return render(request, 'index.html', context)
 
+
 @login_required(login_url='/login/')
 def single(request):
-	is_loggedin = True if request.user.is_authenticated else False
-	is_admin = True if request.user.is_superuser else False
-	username = request.user.username
+    is_loggedin = True if request.user.is_authenticated else False
+    is_admin = True if request.user.is_superuser else False
+    username = request.user.username
 
-	context = {'is_loggedin': is_loggedin, 'is_admin': is_admin, 'username': username}
-	return render(request, 'single.html', context)
-	
+    plot = figure()
+    plot.circle([1, 2], [3, 4])
+
+    script, div = components(plot, CDN)
+
+    context = {'is_loggedin': is_loggedin, 'is_admin': is_admin, 'username': username, 'script': script, 'div': div}
+    return render(request, 'single.html', context)
